4871a4238fe56b9a3c13f9c0e18c0b275d5ce3e3
huangxin <huangxin8697@gmail.com>
2025-01-31 01:50:19 +0800

    feat(tiem-token-test): 自定义 token 生成逻辑
    - 引入自定义 token 生成器，根据不同用户类型生成不同的 token
    - 修改 tokenName 为 "iiib"
    - 保留自定义 token前缀 "Custom "

--- tiem-token-test/src/main/java/com/tiem/token/test/config/TestTokenConfiguration.java
+++ tiem-token-test/src/main/java/com/tiem/token/test/config/TestTokenConfiguration.java
@@ -19,16 +19,23 @@ import java.util.UUID;
  */
 @Configuration
 public class TestTokenConfiguration {
-    
+
     @Bean("tokenConfiguration")
     @Primary
     public TTokenConfiguration tokenConfiguration(
             @Qualifier("defaultTokenConfiguration") DefaultTTokenConfiguration defaultConfig) {
         return TTokenConfiguration.builder()
-            // 使用默认配置的值
-            .tokenName(defaultConfig.getTokenName())
-            // 覆盖需要修改的配置
-            .tokenPrefix("Custom ")
-            .build();
+                // 自定义token生成
+                .tokenGenerator(userObj -> {
+                    if (userObj instanceof CustomUser) {
+                        return ((CustomUser) userObj).getId() + "_" + System.currentTimeMillis();
+                    }
+                    return UUID.randomUUID().toString();
+                })
+                // 使用默认配置的值
+                .tokenName("iiib")
+                // 覆盖需要修改的配置
+                .tokenPrefix("Custom ")
+                .build();
     }
 } 
